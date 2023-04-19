082f23a9c3e7da7f25288ca6e8702714b04ef4c5
moahmed amine mezghich <git@mezghich.com>
2023-04-19 16:20:56 +0100

    la notion de guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,16 @@ import { AddArticleComponent } from './add-article/add-article.component';
 import { ListArticleComponent } from './list-article/list-article.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-
+import { AuthGaurdService } from './services/auth-gaurd.service';
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "app-navbar" },
-  { path: "listProvider", component: ListProviderComponent },
-  { path: "addProvider", component: AddProviderComponent },
-  { path: "updateProvider/:id", component: UpdateProviderComponent },
-  { path: "addArticle", component:AddArticleComponent },
-  { path: "listArticle", component: ListArticleComponent },
+  { path: "listProvider", component: ListProviderComponent, canActivate: [AuthGaurdService] },
+  { path: "addProvider", component: AddProviderComponent, canActivate: [AuthGaurdService] },
+  { path: "updateProvider/:id", component: UpdateProviderComponent , canActivate: [AuthGaurdService]},
+  { path: "addArticle", component:AddArticleComponent , canActivate: [AuthGaurdService]},
+  { path: "listArticle", component: ListArticleComponent, canActivate: [AuthGaurdService] },
   { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent },
+  { path: 'logout', component: LogoutComponent, canActivate: [AuthGaurdService] },
 
 
 
